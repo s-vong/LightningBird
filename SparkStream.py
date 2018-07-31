ca9a3a46ec5e7db17a5408a89fc197c5338789dd
Sam <sdvongsay@gmail.com>
2018-07-30 17:37:37 -0700

    Reorganized functionality into seperate files

--- SparkStream.py
+++ SparkStream.py
@@ -2,41 +2,12 @@ from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import Row, SQLContext 
 import sys
-import requests
 import lightningbird
-import socket
-import json
-
-'''
-def createSocket():
-#    Function to create socket
-#    Must be on local host and on port 9009
-
-    host = socket.gethostname()
-    port = 9009
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    s.listen(1)
-    return s
-'''  
 
 def main():
     #Acquire twitter stream
     stream = lightningbird.run()
 
-    #Create TCP connection
-    host = socket.gethostname()
-    port = 9009
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind( (host, port) )
-
-    #Send data through TCP 
-    for line in stream:
-        twt = json.loads(line)
-        s.send(twt)
-    s.listen(1)
-
-'''
     #Set up Spark
     conf = SparkConf().setAppName("LightningBird")
     sc = SparkContext(conf=conf)
@@ -50,7 +21,6 @@ def main():
     #Start Streaming
     ssc.start()
     ssc.awaitTermination()
-'''
 
 if __name__ == '__main__':
     main()
--- lightningbird.py
+++ lightningbird.py
@@ -1,7 +1,9 @@
 import twitter_cred
 import twitter
+import socket
+import json
 
-def run():
+def get_tweets():
     api = twitter.Api(twitter_cred.CONSUMER_KEY, 
         twitter_cred.CONSUMER_SECRET, 
         twitter_cred.ACCESS_TOKEN, 
@@ -10,3 +12,24 @@ def run():
     tag_lists = ['sdcc', 'San Diego']
     response = api.GetStreamFilter(track = tag_lists)
     return response
+
+def create_tcp():
+    host = socket.gethostname()
+    port = 9009
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind( (host, port) )
+    return s
+
+def send_tweets(response, sock):
+    for line in response:
+        twt = json.dumps(line).encode('utf-8')
+        sock.sendall(twt)
+    sock.listen(1)
+
+def run():
+    response = get_tweets()
+    sock = create_tcp()
+    send_tweets(response, sock)
+    
+if __name__ == '__main__':
+    run()
